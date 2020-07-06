7937b060f14b71758584b3def4adcd35f519775f
konzert <konzert88@hotmail.com>
2020-07-06 18:04:59 -0400

    add landing page

--- client/src/components/home/HomePage.tsx
+++ client/src/components/home/HomePage.tsx
@@ -4,18 +4,22 @@ import '../stylesheets/homepage.css';
 
 const HomePage: React.FunctionComponent = () => {
     return (
-        <div>
+        <div className="landing-page">
             <div className="center-card">
-                <h3>
+                <h1>
                     <span>RE</span>
                     <span>EN</span>
                     <span>GAUGE</span>
-                </h3>
-                <p>Join as a participant?</p>
+                </h1>
+                <p className="participant">Join as a participant?</p>
                 <form action="/quiz">
-                    <input type="text" id="number" name="number" placeholder="Enter your event code"></input>
-                    <input type="submit" value="Submit"></input>
+                    <span className="hashtag">#</span>
+                    <input type="text" id="number" name="number" placeholder="Enter your event code" autoFocus></input>
+                    <input type="submit" id="submit" value="Submit" hidden></input>
                 </form>
+                <p className="signin">
+                    Teacher? <a href="#">Sign in here</a>
+                </p>
             </div>
         </div>
     );

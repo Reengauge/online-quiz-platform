6854267632b790157c8b6990d362788cb79d2ff6
Amulya <amulya@umich.edu>
2020-07-13 19:14:35 -0400

    firebase working status

---
+++ client/src/components/home/PersonalInfoSignup.tsx
@@ -0,0 +1,136 @@
+import React, { useState, useEffect } from 'react';
+import { Button, Card, Form, Text } from 'tabler-react';
+import { Link, useHistory } from 'react-router-dom';
+import { auth, firestore } from '../utils/Firebase';
+import classes from '../stylesheets/SignUp.module.css';
+
+const PersonalInfoSignup = () => {
+    // User Input States
+    const [fullName, setFullName] = useState('');
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+
+    const [errorPersonal, setErrorPersonal] = useState('');
+    const [errorAccount, setErrorAccount] = useState('');
+
+    const history = useHistory();
+
+    // Validate Inputs
+    const isInvalid = !fullName || !email || !password || !confirmPassword;
+
+    const onSignUp = async (e: any) => {
+        e.preventDefault();
+        let isError = false;
+
+        if (isInvalid) {
+            setErrorPersonal("Input fields couldn't be empty");
+            isError = true;
+        }
+
+        const emailPattern = /^\S+@\S+\.\S+/;
+
+        if (isError) {
+            console.log('ending early');
+            return;
+        }
+
+        // all personal errors have been checked and accounted for. We can now reset
+        setErrorPersonal('');
+
+        if (!emailPattern.test(email)) {
+            setErrorAccount('Organizational email is invalid. Please try again');
+            isError = true;
+        }
+
+        if (password !== confirmPassword) {
+            setErrorAccount('Passwords are not matching.');
+            isError = true;
+        }
+
+        if (isError) {
+            console.log('ending early');
+            return;
+        }
+
+        setErrorAccount('');
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((cred) => {
+                const user = cred.user;
+                if (user) {
+                    console.log(user.uid);
+                    // use user safely here
+                    const doc = {
+                        name: fullName,
+                        orgEmail: email,
+                    };
+                    console.log(doc);
+                    firestore.collection('users').doc(user.uid).set(doc);
+                }
+            })
+            .catch((err) => {
+                setErrorAccount(err);
+                console.log(err);
+            });
+
+        // history.push('/signup/apartments');
+        // history.go();
+    };
+
+    // TODO: show this error to the user instead of just logging it
+    useEffect(() => {
+        console.log(errorPersonal);
+    }, [errorPersonal]);
+
+    useEffect(() => {
+        console.log(errorAccount);
+    }, [errorAccount]);
+
+    return (
+        <Card>
+            <Card.Body>
+                <Card.Title>
+                    <span className={classes.NavbarTitle}>Register Reengauge for your Classes</span>
+                </Card.Title>
+                <Form onSubmit={onSignUp}>
+                    <Form.Group label="Account Information">
+                        <Form.Input name="fullname" placeholder="Full Name" onChange={(e: any) => setFullName(e.target.value)} required />
+
+                        <span style={{ color: 'red' }}> {errorPersonal} </span>
+
+                        <Form.Input name="email" type="email" placeholder="Email" onChange={(e: any) => setEmail(e.target.value)} required />
+                        <Form.Input
+                            type="password"
+                            name="password"
+                            placeholder="Password"
+                            onChange={(e: any) => setPassword(e.target.value)}
+                            required
+                        />
+                        <Form.Input
+                            type="password"
+                            name="confirm-password"
+                            placeholder="Confirm Password"
+                            onChange={(e: any) => setConfirmPassword(e.target.value)}
+                            required
+                            invalid={password !== confirmPassword}
+                        />
+                        <span style={{ color: 'red' }}> {errorAccount} </span>
+                    </Form.Group>
+                    <Button block color="primary mt-6 mb-6">
+                        Create Reengauge Account
+                    </Button>
+                    <Text className="text-center">
+                        Have an account?{' '}
+                        <Link to="/signin" className="font-weight-bold">
+                            Sign In
+                        </Link>
+                    </Text>
+                </Form>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default PersonalInfoSignup;
---
+++ client/src/components/utils/Firebase.tsx
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+// import * as c from '../../constants';
+
+// const firebaseConfig = {
+//     apiKey: c.REACT_APP_FIREBASE_KEY,
+//     authDomain: c.REACT_APP_FIREBASE_DOMAIN,
+//     databaseURL: c.REACT_APP_FIREBASE_DATABASE,
+//     projectId: c.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: c.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: c.REACT_APP_FIREBASE_SENDER_ID,
+//     appId: c.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: c.REACT_APP_FIREBASE_MEASUREMENT_ID,
+// };
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCstd06q0sDMKezaUxePgHqkT3MlPN6N-Q',
+    authDomain: 'summer20-sps-8.firebaseapp.com',
+    databaseURL: 'https://summer20-sps-8.firebaseio.com',
+    projectId: 'summer20-sps-8',
+    storageBucket: 'summer20-sps-8.appspot.com',
+    messagingSenderId: '499742742494',
+    appId: '1:499742742494:web:39290693bf54c7472eaf4f',
+    measurementId: 'G-D9MWJECBVS',
+};
+
+firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();

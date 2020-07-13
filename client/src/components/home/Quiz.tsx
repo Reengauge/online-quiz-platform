8d2a0407368e154448f5f7421e256b07abc7989b
Amulya <amulya@umich.edu>
2020-07-13 16:33:25 -0400

    Get the queryParam and get the update QuizObj

--- client/src/components/home/Quiz.tsx
+++ client/src/components/home/Quiz.tsx
@@ -5,14 +5,15 @@ import axios from 'axios';
 
 const endpoint = `http://localhost:3000/api/rooms/12345678/questions`;
 
-const sendGetRequest = () => {
-    return axios({
-        url: endpoint,
-        method: 'get',
-    }).then((response) => {
-        console.log(response);
-        return response.data;
-    });
+var quizObject = {
+    id: '12345678',
+    header: 'No quiz found',
+    subtitle: 'by Professor Jason Hall',
+    introText:
+        'Hi there, its Jason. Can you leave some feedback on our class about the Rate of Return! To edit any of your responses, just click on them.',
+    submitText: 'Thanks for giving us your feedback!',
+
+    questions: [{ question: 'No question found. Please check 404    ', choice: [] }],
 };
 
 function getChoice(choiceNum: any) {
@@ -25,12 +26,28 @@ function getChoice(choiceNum: any) {
     });
 }
 
+function getRoom(choiceNum: any) {
+    return axios({
+        url: 'http://localhost:3000/api/rooms/' + String(choiceNum),
+        method: 'get',
+    }).then((response) => {
+        // console.log(response);
+        return response.data;
+    });
+}
+
+// get Quiz Room Number, and dynamically build the quiz choices
 async function getQuiz(roomNum: any) {
     return axios({
-        url: endpoint,
+        url: `http://localhost:3000/api/rooms/` + roomNum + `/questions`,
         method: 'get',
     }).then(async (response) => {
         var quizObj = response.data;
+        var roomInfo = await getRoom(roomNum);
+        quizObject['subtitle'] = 'by ' + roomInfo['name'].toString();
+        quizObject['introText'] = "Hi there, it's " + roomInfo['presenterId'];
+        quizObject['header'] = 'Dynamic quiz title here';
+
         for (let i = 0; i < quizObj.length; i++) {
             console.log(quizObj[i]);
             quizObj[i]['choices'] = await getChoice(quizObj[i]['questionId']);
@@ -42,150 +59,91 @@ async function getQuiz(roomNum: any) {
 
 const Quiz: React.FunctionComponent = () => {
     const [questions, setQuestions] = useState([]);
-    const [isLoading, setLoading] = useState([]);
-    var quizItem = getQuiz('123456');
+    const [isLoading, setLoading] = useState(true);
 
     var questionsParsed: any = [];
     useEffect(() => {
-        getQuiz('123456').then(async (quiz) => {
+        const queryString = window.location.search;
+        const urlParams = new URLSearchParams(queryString);
+        console.log('questionNumberExtracted: ', urlParams.get('number'));
+
+        getQuiz(urlParams.get('number')).then(async (quiz) => {
             console.log('quizItem promise: ', quiz);
             for (let i = 0; i < quiz.length; i++) {
                 questionsParsed.push({
+                    questionId: quiz[i]['questionId'],
                     question: quiz[i]['questionLabel'],
-                    choices: quiz[i]['choices'].map((choice: any) => {
-                        return choice['choiceLabel'];
-                    }),
+                    choices: quiz[i]['choices'],
                 });
             }
             console.log('questionsParsed: ', questionsParsed);
             setQuestions(questionsParsed);
+            quizObject['questions'] = questionsParsed;
+            console.log('updated quiz object');
+            setLoading(false);
         });
         // return () => {
 
         // };
     }, []);
 
-    // console.log('quizItem: ', quizItem);
-
     //unit tests for empty string, multiple choice, free responses, only uses specific part of the questions response data, ensure page load happens after,
-    // question.choices.map((choice, index2) => (
-    //     <Option value={index2}>{choice}</Option>
-    // ))
-
-    // function LoadingIndicator({ isLoading: any }) {
-    //     return <div>{isLoading && <p>Loading...</p>}</div>;
-    // }
-
-    const quizObject = {
-        id: '51321',
-        header: 'FIN 300 - Class 3 Survey - Understanding Check In',
-        subtitle: 'by Professor Jason Hall',
-        introText:
-            'Hi there, its Jason. Can you leave some feedback on our class about the Rate of Return! To edit any of your responses, just click on them.',
-        submitText: 'Thanks for giving us your feedback!',
-        // questions: [
-        //     {
-        //         question:
-        //             'You need to accumulate $25,000 in 10 years. How much will you have to invest right now if your rate of return is 6% compounded semi-annually?',
-        //         choices: ['$11,409.79', '$11,579.97', '$13,841.98', '$13,960.24'],
-        //     },
-
-        //     {
-        //         question: 'If you were quizzed on Rate of return tomorrow, how would you feel?',
-        //         choices: ['5 stars', '3 stars', '1 star'],
-        //     },
-
-        //     {
-        //         question: 'What is your name?',
-        //         choices: [],
-        //     },
-        // ],
-        questions: questionsParsed,
-    };
-
-    const GeneratedConvesationalForm = () => {
-        var questionsParsed: any = [];
-
-        getQuiz('123456').then(async (quiz) => {
-            console.log('quizItem promise: ', quiz);
-            for (let i = 0; i < quiz.length; i++) {
-                questionsParsed.push({
-                    question: quiz[i]['questionLabel'],
-                    choices: quiz[i]['choices'].map((choice: any) => {
-                        return choice['choiceLabel'];
-                    }),
-                });
-            }
-            console.log('questionsParsed: ', questionsParsed);
-        });
-
-        return (
-            <Conversation
-                className="conversation"
-                onSubmit={(Response: any) => {
-                    console.log(Response);
-                    console.log('will post to: ', quizObject.id);
-                }}
-                chatOptions={{
-                    introText: quizObject.introText,
-                    submitText: quizObject.submitText,
-                }}
-            >
-                {questionsParsed.map((localQuestion: any, index: any) => {
-                    if (localQuestion.choices.length === 0) {
-                        return <Question id="index"> {localQuestion.question} </Question>;
-                    }
-                    return (
-                        <Select id={index} question={localQuestion.question}>
-                            {localQuestion.choices.map((choice: any, index2: any) => (
-                                <Option value={index2}>{choice}</Option>
-                            ))}
-                        </Select>
-                    );
-                })}
-            </Conversation>
-        );
-    };
 
     return (
         <div>
-            <div
-                className="chat-header"
-                style={{ backgroundColor: '#5344B2', height: '100px', color: 'white', zIndex: 9999999999, borderRadius: '10px 10px 0px 0px' }}
-            >
-                <h3>{quizObject.header}</h3>
-                <p>{quizObject.subtitle}</p>
-            </div>
-            <div>
-                <button onClick={() => getQuiz('123456')}>Get Questions for quiz 1</button>
-                <button onClick={() => getChoice('1')}>Get choices for question 1</button>
-            </div>
-            <div className="chat-screen">
-                <Conversation
-                    className="conversation"
-                    onSubmit={(Response: any) => {
-                        console.log(Response);
-                        console.log('will post to: ', quizObject.id);
-                    }}
-                    chatOptions={{
-                        introText: quizObject.introText,
-                        submitText: quizObject.submitText,
-                    }}
-                >
-                    {quizObject.questions.map((question: any, index: any) => {
-                        if (question.choices.length === 0) {
-                            return <Question id="index"> {question.question} </Question>;
-                        }
-                        return (
-                            <Select id={index} question={question.question}>
-                                {question.choices.map((choice: any, index2: any) => (
-                                    <Option value={index2}>{choice}</Option>
-                                ))}
-                            </Select>
-                        );
-                    })}
-                </Conversation>
-            </div>
+            {isLoading ? (
+                <>
+                    <button
+                        onClick={() => {
+                            console.log('thequizobject: ', quizObject);
+                            setLoading(false);
+                        }}
+                    >
+                        Finish loading for Quiz {quizObject['id']}
+                    </button>
+                </>
+            ) : (
+                <>
+                    <div
+                        className="chat-header"
+                        style={{ backgroundColor: '#5344B2', height: '100px', color: 'white', zIndex: 9999999999, borderRadius: '10px 10px 0px 0px' }}
+                    >
+                        <h3>{quizObject.header}</h3>
+                        <p>{quizObject.subtitle}</p>
+                    </div>
+                    <div>
+                        <button onClick={() => getQuiz('123456')}>Get Questions for quiz 1</button>
+                        <button onClick={() => getChoice('1')}>Get choices for question 1</button>
+                        <button onClick={() => console.log(quizObject)}>Quiz object console log</button>
+                    </div>
+                    <div className="chat-screen">
+                        <Conversation
+                            className="conversation"
+                            onSubmit={(Response: any) => {
+                                console.log(Response);
+                                console.log('will post to: ', quizObject.id);
+                            }}
+                            chatOptions={{
+                                introText: quizObject.introText,
+                                submitText: quizObject.submitText,
+                            }}
+                        >
+                            {quizObject.questions.map((question: any, index: any) => {
+                                if (question.choices.length === 0) {
+                                    return <Question id={question.questionId.toString()}> {question.question.toString()} </Question>;
+                                }
+                                return (
+                                    <Select id={question.questionId.toString()} question={question.question}>
+                                        {question.choices.map((choice: any, index2: any) => (
+                                            <Option value={index2.toString()}>{choice.choiceLabel.toString()}</Option>
+                                        ))}
+                                    </Select>
+                                );
+                            })}
+                        </Conversation>
+                    </div>
+                </>
+            )}
         </div>
     );
 };

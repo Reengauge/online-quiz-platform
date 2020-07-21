8e1032635e77f869fe9d4d768581ccc156fa0859
Amulya <amulya@umich.edu>
2020-07-20 21:00:49 -0400

    dynamic login dynamic name

--- client/src/components/utils/Firebase.tsx
+++ client/src/components/utils/Firebase.tsx
@@ -28,3 +28,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
+export const provider = new firebase.auth.GoogleAuthProvider();
